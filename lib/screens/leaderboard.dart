8b047ca912da142b8a27ef06ba4903f1965a2f73
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-08-19 04:35:00 +1200

    Added starting files for menu screens.

---
+++ lib/screens/leaderboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeaderScreen extends StatelessWidget {
+  const LeaderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
