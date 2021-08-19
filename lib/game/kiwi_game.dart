498cadcbdacf2b1175f349ecc204c8bef13f59ec
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-08-19 16:28:28 +1200

    Added movement and clamp using game_size_aware mixin

--- lib/game/kiwi_game.dart
+++ lib/game/kiwi_game.dart
@@ -7,19 +7,23 @@ import 'package:flutter_game/game/widgets/kiwi.dart';
 
 import 'game_size_aware.dart';
 
-class KiwiGame extends BaseGame with PanDetector {
+class KiwiGame extends BaseGame with TapDetector {
   bool _isAlreadyLoaded = false;
+  bool _bothDirectionsPressed = false;
+  bool _leftDirectionPressed = false;
+  bool _rightDirectionPressed = false;
+  late Kiwi _kiwi;
 
   @override
   Future<void> onLoad() async {
     if (!_isAlreadyLoaded) {
-      Kiwi kiwi = Kiwi(
-        sprite: await Sprite.load('kiwi_sprite.jpg'),
-        size: Vector2(100, 100),
-        position: viewport.canvasSize / 2,
-      );
-      kiwi.anchor = Anchor.center;
-      add(kiwi);
+      _kiwi = Kiwi(
+          sprite: await Sprite.load('kiwi_sprite.jpg'),
+          size: Vector2(100, 100),
+          position:
+              Vector2(viewport.canvasSize.x / 2, viewport.canvasSize.y / 3));
+      _kiwi.anchor = Anchor.center;
+      add(_kiwi);
       _isAlreadyLoaded = true;
     }
   }
@@ -35,7 +39,57 @@ class KiwiGame extends BaseGame with PanDetector {
   }
 
   @override
+  void prepare(Component c) {
+    super.prepare(c);
+
+    // If the component being prepared is of type KnowsGameSize,
+    // call onResize() on it so that it stores the current game screen size.
+    if (c is GameSizeAware) {
+      c.onResize(this.size);
+    }
+  }
+
+  @override
   void render(Canvas canvas) {
     super.render(canvas);
   }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    _kiwi.update(dt);
+  }
+
+  @override
+  void onTapDown(TapDownInfo event) {
+    print("Player tap down on ${event.eventPosition.game.x}");
+    if (!(_rightDirectionPressed && _leftDirectionPressed)) {
+      if (event.eventPosition.game.x < getMiddlePoint()) {
+        print("Kiwi going left.");
+        _kiwi.setMoveDirection(Vector2(-1, 0));
+      } else if ((event.eventPosition.game.x > getMiddlePoint()) &&
+          !_leftDirectionPressed) {
+        print("Kiwi going right.");
+        _kiwi.setMoveDirection(Vector2(1, 0));
+      }
+    }
+  }
+
+  @override
+  void onTapUp(TapUpInfo event) {
+    _kiwi.setMoveDirection(Vector2.zero());
+    _leftDirectionPressed = false;
+    _rightDirectionPressed = false;
+    _bothDirectionsPressed = false;
+  }
+
+  @override
+  void onTapCancel() {
+    _kiwi.setMoveDirection(Vector2.zero());
+    _leftDirectionPressed = false;
+    _rightDirectionPressed = false;
+    _bothDirectionsPressed = false;
+  }
+
+  double getMiddlePoint() => viewport.canvasSize.x / 2;
 }
--- lib/game/widgets/kiwi.dart
+++ lib/game/widgets/kiwi.dart
@@ -1,11 +1,10 @@
 import 'package:flame/components.dart';
 import 'package:flutter_game/game/game_size_aware.dart';
 
-import '../kiwi_game.dart';
-
-class Kiwi extends SpriteComponent {
-  Vector2 _moveDirection = Vector2.zero();
-  double _speed = 300;
+class Kiwi extends SpriteComponent with GameSizeAware {
+  // The kiwi is initialised in the center with no motion.
+  Vector2 _horizontalMoveDirection = Vector2.zero();
+  double _horizontalSpeed = 100;
 
   Kiwi({
     Sprite? sprite,
@@ -14,13 +13,23 @@ class Kiwi extends SpriteComponent {
   }) : super(sprite: sprite, position: position, size: size);
 
   @override
+  void onMount() {
+    super.onMount();
+  }
+
+  @override
   void update(double dt) {
     super.update(dt);
+    this.position +=
+        _horizontalMoveDirection.normalized() * _horizontalSpeed * dt;
 
-    this.position += _moveDirection.normalized() * _speed * dt;
+    this.position.clamp(
+          Vector2.zero() + this.size / 2,
+          gameSize - this.size / 2,
+        );
   }
 
   void setMoveDirection(Vector2 newMoveDirection) {
-    _moveDirection = newMoveDirection;
+    _horizontalMoveDirection = newMoveDirection;
   }
 }
