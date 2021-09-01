1ee17170a63c065d1c0ff26d1e04f732b6c76445
MasaGt <MasaGt@users.noreply.github.com>
2021-09-01 23:21:54 +1200

    Implemented retrieving data from Hive

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,65 +1,93 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_game/screens/score_item.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-class LeaderScreen extends StatelessWidget {
-  const LeaderScreen({Key? key}) : super(key: key);
+class LeaderScreen extends StatefulWidget {
 
   @override
-  Widget build(BuildContext context) {
-    const _iconColors = [Colors.amberAccent, Colors.blueGrey, Colors.brown];
-    List<listItem> items = populateData();
-
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Flexible(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(10),
-                itemCount: items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: index <= 2 ? Icon(Icons.emoji_events, color: _iconColors[index],) : Text((index + 1).toString()),
-                    title: Text(items[index]._userNm),
-                    trailing: Text(items[index]._score.toString()),
-                  );
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                    onPressed: () => {Navigator.pop(context)},
-                    child: Text("Go back")
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  List<listItem> populateData() {
-    final List<listItem> items  = <listItem>[];
-    for (int i = 1; i <= 10; i++) {
-      listItem item = new listItem("user$i", (10-i)*1000);
-      items.add(item);
-    }
-    return items;
+  State<StatefulWidget> createState() {
+    return _LeaderScreenState();
   }
 }
 
-class listItem {
-  String _userNm;
-  int _score;
+class _LeaderScreenState extends State<LeaderScreen> {
 
-  listItem(this._userNm, this._score);
+  late Box<ScoreItem>  scoreBox;
+  final String BOXNAME = "leaderboard";
+  static const _iconColors = [Colors.amberAccent, Colors.blueGrey, Colors.brown];
 
-  //getter
-  String get name => _userNm;
-  int get score => _score;
+  @override
+  void initState() {
+    super.initState();
+
+    scoreBox = Hive.box<ScoreItem>(BOXNAME);
+
+    //TODO remove this data population before launching the app
+    bool debug = true;
+    if (debug) {
+      print("isEmptyCheck");
+
+      if (!scoreBox.isEmpty) {
+        print("Clear all");
+        scoreBox.deleteAll(scoreBox.keys);
+      }
+
+      print("populate");
+      for (int i = 1; i <= 10; i++) {
+        print("$i times");
+        ScoreItem item = new ScoreItem(i-1, "user$i", (10-i)*1000);
+        scoreBox.add(item);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Scaffold(
+        body: ValueListenableBuilder(
+          valueListenable: scoreBox.listenable(),
+          builder: (BuildContext context, value, Widget? child) {
+            var scores = scoreBox.values.toList().cast<ScoreItem>();
+            scores.sort((a,b) => a.rank.compareTo(b.rank));
+            int len = scoreBox.length;
+            print("length $len");
+
+            return
+              Column(
+                  children: [
+                    Flexible(
+                      child: ListView.builder(
+                        padding: const EdgeInsets.all(10),
+                        itemCount: scores.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          var entry = scores[index];
+                          return ListTile(
+                            leading: index <= 2 ? Icon(Icons.emoji_events, color: _iconColors[index],) : Text((index + 1).toString()),
+                            title: Text(entry.userNm),
+                            trailing: Text(entry.score.toString()),
+                          );
+                        },
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () => {Navigator.of(context).pop()},
+                            child: Text("Go back")
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+          },
+        ),
+      );
+  }
+
 }
 
 
