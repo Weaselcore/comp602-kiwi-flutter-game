51b1d8ec975a8d43a49c3d105537c862988f7346
MasaGt <MasaGt@users.noreply.github.com>
2021-09-01 23:18:15 +1200

    Register datamodel to Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_game/screens/main_menu.dart';
+import 'package:flutter_game/screens/score_item.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
+
   if(!kIsWeb) {
+    WidgetsFlutterBinding.ensureInitialized();
     var dir = await getApplicationDocumentsDirectory();
     Hive.init(dir.path);
   }
+
+  Hive.registerAdapter(ScoreItemAdapter());
+  Hive.openBox<ScoreItem>("leaderboard");
+
   runApp(MyApp());
 }
 
