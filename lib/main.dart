420ed6e380dfbdbb0f9c81f0321c4e9d7f1fda02
MasaGt <MasaGt@users.noreply.github.com>
2021-09-01 14:28:34 +1200

    added hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_game/screens/main_menu.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  if(!kIsWeb) {
+    var dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+  }
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
