a837237e4fb1cda8c9cfc48423ac83a17f1f5a07
MasaGt <MasaGt@users.noreply.github.com>
2021-09-12 23:14:56 +1200

    Added google authentication check

--- lib/screens/dao/remote_score_dao.dart
+++ lib/screens/dao/remote_score_dao.dart
@@ -1,11 +1,9 @@
-import 'dart:collection';
-import 'dart:ffi';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_game/screens/dao/local_score_dao.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hive/hive.dart';
 
 import '../score_item.dart';
@@ -25,7 +23,22 @@ class RemoteScoreDao {
     var connectivityResult = await (Connectivity().checkConnectivity());
     if (connectivityResult == ConnectivityResult.mobile || connectivityResult == ConnectivityResult.wifi) {
       // have internet access
-      //TODO google authentication check
+      //authentication check
+      //signInSilently() tries to get previous login info without a dialog.
+      final user = await GoogleSignIn().signInSilently();
+
+      if (user != null) {
+        final GoogleSignInAuthentication googleAuth = await user.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        FirebaseAuth.instance.signInWithCredential(credential);
+      } else {
+        //user who does not have google account.
+        FirebaseAuth.instance.signInAnonymously();
+      }
+
       //fetch local data
       List<ScoreItem> scores = _localDao.getAll();
       var data = [];
