55c0e28c1b92d90f68039008959fb9c588ec84fe
Joseph Grant Busico <busicogrant@gmail.com>
2021-09-17 17:45:08 +1200

    fixing

---
+++ lib/models/subcategory.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class subcategory {
+
+  subcategory({
+    String name,
+    String icon,
+    Color color,
+    String imgName
+}): super(
+    name: name,
+    icon: icon,
+    color: color,
+    imgName: imgName
+  );
+}
--- lib/screens/shop.dart
+++ lib/screens/shop.dart
@@ -5,11 +5,10 @@ import 'package:flutter_game/main.dart';
 class ShopScreen extends StatelessWidget {
    const ShopScreen({Key? key}) : super(key: key);
 
-  get coins => 1000; //Local get and must be update when after each user game.
+   static const IconData attach_money = IconData(0xe0b2, fontFamily: 'MaterialIcons');
+
+  get coins => 2000; //Local get and must be update when after each user game.
   get item => 100;
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +16,8 @@ class ShopScreen extends StatelessWidget {
     var image = new Image(image: assetsImage, width:48.0, height: 48.0);
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.brown[500],
+            backgroundColor: Colors.white70,
+            //AppBar
             appBar: AppBar(
               backgroundColor: Colors.brown,
               leading: Icon(Icons.store_rounded),
@@ -25,17 +25,30 @@ class ShopScreen extends StatelessWidget {
                 'Shop',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
+              //Add an icon for ads
+              /*
+              actions: [
+                Container(
+                  margin: EdgeInsets.only(right:0 ),
+                  //padding: EdgeInsets.all(10),
+                    child: Image.asset('assets/images/ads.png')
+
+                )
+              ],*/
             ),
+            //End of AppBar
+
+            //Body Content
             body: Center(
               child: Container(
                   padding: EdgeInsets.all(20.0),
                   child: Column(children: <Widget>[
                     Container(
                         width: double.infinity,
-                        height: 200,
+                        height: 80,
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: Colors.brown[200]),
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.brown[300]),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
@@ -45,9 +58,9 @@ class ShopScreen extends StatelessWidget {
                                     fontSize: 30,
                                     color: Colors.black)),
                             SizedBox(
-                              height: 30,
+                              height: 0,
                             ),
-                            Container(
+                            /*Container(
                                 height: 50,
                                 margin: EdgeInsets.symmetric(horizontal: 40.0),
                                 decoration: BoxDecoration(
@@ -62,14 +75,14 @@ class ShopScreen extends StatelessWidget {
                                       ),
                                     )),onTap:(){print('button clicked Ads to play  in queue');} //Should open a page for coins
                                     )
-                            ),
+                            ),*/
                           ],
                         )),
                     SizedBox(
-                      height: 10,
+                      height: 0,
                     ),
                     Expanded(
-                      child: GestureDetector(child: GridView.count(
+                      child: GridView.count(
                           scrollDirection: Axis.vertical,
                           shrinkWrap: true,
                           crossAxisCount: 2,
@@ -77,7 +90,8 @@ class ShopScreen extends StatelessWidget {
                           crossAxisSpacing: 20,
                           mainAxisSpacing: 20,
                           children: [
-                            Container( //put this in the middle
+                            //Get rid of Box Header
+                            /*Container( //put this in the middle
                                 width: double.infinity,
                                 height: 50,
                                 padding: EdgeInsets.all(10.0),
@@ -90,7 +104,7 @@ class ShopScreen extends StatelessWidget {
                                   Text("Kiwi Skins"),
                                 ],
                               )
-                            ),
+                            ),*/
                             Image.asset(//grey
                               'assets/images/kiwi_sprite_grey.png',
                             ),
@@ -103,7 +117,7 @@ class ShopScreen extends StatelessWidget {
                             Image.asset(//yellow
                               'assets/images/kiwi_sprite_yellow.png',
                             ),
-                            Container( //put this in the middle
+                              /*Container( //put this in the middle          [optional items]
                                 width: double.infinity,
                                 height: 50,
                                 padding: EdgeInsets.all(10.0),
@@ -117,7 +131,7 @@ class ShopScreen extends StatelessWidget {
                                   ],
                                 )
                             ),
-                            Image.asset(//heart
+                           Image.asset(//heart        [optional items]
                               'assets/images/heart.png',
                             ),
                             Image.asset(//Caterpillar
@@ -131,27 +145,20 @@ class ShopScreen extends StatelessWidget {
                             ),
                             Image.asset(//Worm
                               'assets/images/Worm.png'
-                            )
+                            )*/
                             ]
-                      ),
-                    onTap:(){
-                        const snackBar = SnackBar(content: Text('Item bought'));
-
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                        "$coins - $item";
-                        print('I clicked it, Coin value update');
-                        return coins;
-                        },)
-                    )]
+                          ),
+                        )
+                    ]
                   )
               ),
-                )
+            )
             )
         );
   }
 }
  void main() {
-  int coins = 1000;
+  int coins = 2000;
 
 
  }
