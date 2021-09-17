d74b76b6c793a4538b1416f3d69bfbf587038085
Joseph Grant Busico <busicogrant@gmail.com>
2021-09-17 22:39:33 +1200

    Button error

--- lib/screens/shop.dart
+++ lib/screens/shop.dart
@@ -3,15 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_game/main.dart';
 import 'package:hive/hive.dart';
 
-class ShopScreen extends StatelessWidget {
-   const ShopScreen({Key? key}) : super(key: key);
+class ShopScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ShopScreenState();
+  }
+}
 
-   static const IconData attach_money = IconData(0xe0b2, fontFamily: 'MaterialIcons');
+class _ShopScreenState extends State<ShopScreen> {
 
-  get coins => 2000; //Local get and must be update when after each user game.
-  get item => 100;
+  late Box _box;
+  late int _coin;
+  late String _skin;
 
-  get configBox => null;
+  @override
+  void initState(){
+    super.initState();
+    _box = Hive.box("config");
+
+    _coin = _box.get("coin");
+    _skin = _box.get("skin");
+  }
+
+  int itemscost = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +48,7 @@ class ShopScreen extends StatelessWidget {
 
             //Body Content
             body: Center(
-              child: Container(
+                child: Container(
                   padding: EdgeInsets.all(20.0),
                   child: Column(children: <Widget>[
                     Container(
@@ -46,7 +60,7 @@ class ShopScreen extends StatelessWidget {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            Text("Kiwi Coins = $coins",
+                            Text("Kiwi Coins = $_coin",
                                 style: TextStyle(
                                     fontWeight: FontWeight.bold,
                                     fontSize: 30,
@@ -65,71 +79,65 @@ class ShopScreen extends StatelessWidget {
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10.0),
                             color: Colors.brown[300]),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text("Skins",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.black))
-                        ]
-                      )
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Skins",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.black))
+                            ]
+                        )
                     ),
                     Expanded(
                         child: GestureDetector(child: GridView.count(
                           //scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          crossAxisCount: 2,
-                          padding: EdgeInsets.all(20.0),
-                          crossAxisSpacing: 20,
-                          mainAxisSpacing: 20,
-                          children: [
-                            Image.asset(//grey
-                              'assets/images/kiwi_sprite_grey.png',
-                            ),
-                            Image.asset(//purple
-                              'assets/images/kiwi_sprite_purple.png',
-                            ),
-                            Image.asset(//red
-                              'assets/images/kiwi_sprite_red.png',
-                            ),
-                            Image.asset(//yellow
-                              'assets/images/kiwi_sprite_yellow.png',
-                            ),
+                            shrinkWrap: true,
+                            crossAxisCount: 2,
+                            padding: EdgeInsets.all(20.0),
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20,
+                            children: [
+                              Image.asset(//grey
+                                'assets/images/kiwi_sprite_grey.png',
+                              ),
+                              Image.asset(//purple
+                                'assets/images/kiwi_sprite_purple.png',
+                              ),
+                              Image.asset(//red
+                                'assets/images/kiwi_sprite_red.png',
+                              ),
+                              Image.asset(//yellow
+                                'assets/images/kiwi_sprite_yellow.png',
+                              ),
                             ]
-                          ),
-                        onTap:() async {
-                          await configBox.put("coin", coins - item);
-                          //await configBox.put("skin", actual value);
-                          print('tapped');
-                        }
+                        ),
+                            onTap:() async {
+                              await _box.put("coin", _coin - itemscost);
+                              await _box.put("skin", "kiwi_sprite_purple.png");
+                              print('tapped');
+                            }
                         )
-                      ),
+                    ),
                     //Back Button
-                  Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () => {Navigator.of(context).pop()},
-                        child: Text("Go back")),
-                    ]
-                  )
-              ]),
-            )
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () => {Navigator.of(context).pop()},
+                              child: Text("Go back")),
+                        ]
+                    )
+                  ]),
+                )
             )
         ));
   }
+
 }
- void main() {
 
-  Box configBox = Hive.box("config");
-
-  int coin = configBox .get("coin");
-  String skin = configBox .get("skin");
+  get coins => null; //Local get and must be update when after each user game.
+  get item => 100;
 
 
-
- }
-//TO DO: Add buttons to the Items and functions to buttons
-//Shop itemS: Kiwifruit, bugs
