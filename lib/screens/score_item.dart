b1954b7977550e2bf31d1d0661c50e4607ba9297
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-09-24 17:39:56 +1200

    Formatting changes to try to fix flutter runner

--- lib/screens/score_item.dart
+++ lib/screens/score_item.dart
@@ -1,12 +1,10 @@
-
 import 'package:hive/hive.dart';
 
 part "score_item.g.dart";
 
 // model class for score
 @HiveType(typeId: 0)
-class ScoreItem extends HiveObject{
-
+class ScoreItem extends HiveObject {
   @HiveField(1)
   String userNm;
 
